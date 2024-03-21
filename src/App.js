8194d2bdb822477ac25005134b96183aeafc7f1b
tranthang <tranthangusername@gmail.com>
2024-03-21 22:22:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Navbar,Footer} from './components';
+import './output.css';
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import AllProducts from './Pages/AllProducts';
+import { CartContextProvider } from './components/CartContext';
+import Cart from './Pages/Cart';
+import ProductDetail from './Pages/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-[#ebebeb]'>
+      <Router>
+        <CartContextProvider>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<Home/>}> </Route>
+              <Route path='/all-products' element={<AllProducts/>}> </Route>
+              <Route path='/cart' element={<Cart/>}> </Route>
+              <Route path='/product/:id' element={<ProductDetail/>} />
+            </Routes>
+          <Footer />
+        </CartContextProvider>
+      </Router>
     </div>
   );
 }
